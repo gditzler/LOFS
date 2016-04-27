ef38b564fee903039c3ace2969c2f46631a4f8ef
Gregory Ditzler <gregory.ditzler@gmail.com>
2016-04-27 09:33:27 -0700

    cleaned up... updated doc... more consistant

--- correlation_measure/fisher-z-test/my_cond_indep_fisher_z.m
+++ correlation_measure/fisher-z-test/my_cond_indep_fisher_z.m
@@ -1,6 +1,6 @@
 function [CI, r, p] = my_cond_indep_fisher_z(data,X, Y, S,N, alpha)
-% COND_INDEP_FISHER_Z Test if X indep Y given Z using Fisher's Z test
-% CI = cond_indep_fisher_z(X, Y, S, C, N, alpha)
+% MY_COND_INDEP_FISHER_Z Test if X indep Y given Z using Fisher's Z test
+% [CI, r, p] = my_cond_indep_fisher_z(data,X, Y, S,N, alpha)
 %
 % C is the covariance (or correlation) matrix
 % N is the sample size
@@ -8,13 +8,15 @@ function [CI, r, p] = my_cond_indep_fisher_z(data,X, Y, S,N, alpha)
 %
 % See p133 of T. Anderson, "An Intro. to Multivariate Statistical Analysis", 1984
 
-if nargin < 6, alpha = 0.05; end
+if nargin < 6, 
+  alpha = 0.05; 
+end
 
-C=cov([data(:,[X,Y,S])]);
-size_C=size(C,2);
-X1=1;
-Y1=2;
-S1=[3:size_C];
+C = cov(data(:,[X,Y,S]));
+size_C = size(C,2);
+X1 = 1;
+Y1 = 2;
+S1 = 3:size_C;
 
 
 r = partial_corr_coef(C, X1, Y1, S1);
@@ -29,7 +31,7 @@ else % reject the null hypothesis that rho = 0
   CI = 0;
 end
 p = normcdf(W);
-r=abs(r);
+r = abs(r);
 %p = mynormcdf(W);
 
 %%%%%%%%%
@@ -52,17 +54,17 @@ function p = normcdf(x,mu,sigma)
 %   $Revision: 1.1.1.1 $  $Date: 2005/04/26 02:29:18 $
 
 if nargin < 3, 
-    sigma = 1;
+  sigma = 1;
 end
 
 if nargin < 2;
-    mu = 0;
+  mu = 0;
 end
 
 [errorcode x mu sigma] = distchck(3,x,mu,sigma);
 
 if errorcode > 0
-    error('Requires non-scalar arguments to match in size.');
+  error('Requires non-scalar arguments to match in size.');
 end
 
 %   Initialize P to zero.
@@ -71,25 +73,25 @@ p = zeros(size(x));
 % Return NaN if SIGMA is not positive.
 k1 = find(sigma <= 0);
 if any(k1)
-    tmp   = NaN;
-    p(k1) = tmp(ones(size(k1))); 
+  tmp   = NaN;
+  p(k1) = tmp(ones(size(k1))); 
 end
 
 % Express normal CDF in terms of the error function.
 k = find(sigma > 0);
 if any(k)
-    p(k) = 0.5 * erfc( - (x(k) - mu(k)) ./ (sigma(k) * sqrt(2)));
+  p(k) = 0.5 * erfc( - (x(k) - mu(k)) ./ (sigma(k) * sqrt(2)));
 end
 
 % Make sure that round-off errors never make P greater than 1.
 k2 = find(p > 1);
 if any(k2)
-    p(k2) = ones(size(k2));
+  p(k2) = ones(size(k2));
 end
 
 %%%%%%%%
 
-function x = norminv(p,mu,sigma);
+function x = norminv(p,mu,sigma)
 %NORMINV Inverse of the normal cumulative distribution function (cdf).
 %   X = NORMINV(P,MU,SIGMA) finds the inverse of the normal cdf with
 %   mean, MU, and standard deviation, SIGMA.
@@ -107,17 +109,17 @@ function x = norminv(p,mu,sigma);
 %   $Revision: 1.1.1.1 $  $Date: 2005/04/26 02:29:18 $
 
 if nargin < 3, 
-    sigma = 1;
+  sigma = 1;
 end
 
 if nargin < 2;
-    mu = 0;
+  mu = 0;
 end
 
 [errorcode p mu sigma] = distchck(3,p,mu,sigma);
 
 if errorcode > 0
-    error('Requires non-scalar arguments to match in size.');
+  error('Requires non-scalar arguments to match in size.');
 end
 
 % Allocate space for x.
@@ -126,25 +128,25 @@ x = zeros(size(p));
 % Return NaN if the arguments are outside their respective limits.
 k = find(sigma <= 0 | p < 0 | p > 1);
 if any(k)
-    tmp  = NaN;
-    x(k) = tmp(ones(size(k))); 
+  tmp  = NaN;
+  x(k) = tmp(ones(size(k))); 
 end
 
 % Put in the correct values when P is either 0 or 1.
 k = find(p == 0);
 if any(k)
-    tmp  = Inf;
-    x(k) = -tmp(ones(size(k)));
+  tmp  = Inf;
+  x(k) = -tmp(ones(size(k)));
 end
 
 k = find(p == 1);
 if any(k)
-    tmp  = Inf;
-    x(k) = tmp(ones(size(k))); 
+  tmp  = Inf;
+  x(k) = tmp(ones(size(k))); 
 end
 
 % Compute the inverse function for the intermediate values.
 k = find(p > 0  &  p < 1 & sigma > 0);
 if any(k),
-    x(k) = sqrt(2) * sigma(k) .* erfinv(2 * p(k) - 1) + mu(k);
+  x(k) = sqrt(2) * sigma(k) .* erfinv(2 * p(k) - 1) + mu(k);
 end
